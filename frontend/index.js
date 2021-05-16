89b9b5be43655f95ebbc39c0bbdde0a1018c9147
normuradoff <normuradoff1@gmail.com>
2021-05-16 23:25:33 +0500

    fix: enhance getter function

--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,18 @@
 const get = async (slug) => {
   response = await fetch(`http://blogpost.sbuy.uz/api/blog/${slug}`);
   if (response.ok) {
-    console.log(response);
+    return response.json();
   } else
     console.log(
       new Error(
-        "Something went wrong in getting a response! Please, reload the page."
+        "Something went wrong in getting a response. Please, try again!."
       )
     );
-
-  return response.json();
 };
 
 const setCategories = async () => {
   const categoriesContainer = document.getElementById("categories");
   let { categories } = await get("get_categories");
-  console.log(categories);
   const newList = categories.forEach(({ id, title }) => {
     const category = document.createElement("div");
     category.className = "footer-link";
@@ -24,9 +21,7 @@ const setCategories = async () => {
     anchor.innerHTML = title;
     category.appendChild(anchor);
     categoriesContainer.appendChild(category);
-    console.log(category);
   });
-  console.dir(newList);
 };
 
 setCategories();
