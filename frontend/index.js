daf1418e0e2d81277c01bdf1042d8636edf44a71
normuradoff <normuradoff1@gmail.com>
2021-05-16 23:21:35 +0500

    feat(functions): add a function for setting categories list in the navbar

--- frontend/index.js
+++ frontend/index.js
@@ -11,3 +11,22 @@ const get = async (slug) => {
 
   return response.json();
 };
+
+const setCategories = async () => {
+  const categoriesContainer = document.getElementById("categories");
+  let { categories } = await get("get_categories");
+  console.log(categories);
+  const newList = categories.forEach(({ id, title }) => {
+    const category = document.createElement("div");
+    category.className = "footer-link";
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", `/${title}`);
+    anchor.innerHTML = title;
+    category.appendChild(anchor);
+    categoriesContainer.appendChild(category);
+    console.log(category);
+  });
+  console.dir(newList);
+};
+
+setCategories();
