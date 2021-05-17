4d2a81803ed44fb33058df6b4f9f8559ac9c6484
normuradoff <normuradoff1@gmail.com>
2021-05-17 21:23:27 +0500

    feat(functions): add a function for loading posts and setting them into HTML

--- frontend/index.js
+++ frontend/index.js
@@ -13,7 +13,7 @@ const get = async (slug) => {
 const setCategories = async () => {
   const categoriesContainer = document.getElementById("categories");
   let { categories } = await get("get_categories");
-  const newList = categories.forEach(({ id, title }) => {
+  categories.forEach(({ id, title }) => {
     const category = document.createElement("div");
     category.className = "footer-link";
     const anchor = document.createElement("a");
@@ -24,4 +24,27 @@ const setCategories = async () => {
   });
 };
 
+const setPosts = async () => {
+  const postsContainers = document.getElementsByClassName("miniBlogHolder");
+  const cardTemplate = document.getElementsByTagName("template")[0];
+  let {
+    posts: { data, first_page_url, last_page_url, last_page, prev_page_url },
+  } = await get("get_posts");
+  console.log(data);
+  data.forEach(({ title, author, body, views }, index) => {
+    const card = cardTemplate.content.cloneNode(true);
+    const anchor = card.querySelector("a");
+    anchor.setAttribute("href", "title");
+    const titleHtml = card.querySelector("h2");
+    title.innerHTML = title;
+    const viewsHtml = card.querySelector(".views");
+    views.innerHTML = views;
+    const authorHtml = card.querySelector(".author");
+    author.innerHTML = author;
+    console.log(card);
+    postsContainers[0].appendChild(card);
+  });
+};
+
 setCategories();
+setPosts();
