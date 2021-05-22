91dcfd8049aa3883e9fd861794e8d1a5163afffe
normuradoff <normuradoff1@gmail.com>
2021-05-22 13:30:34 +0500

    feat(js): dynamically set a data of post

--- frontend/js/post.js
+++ frontend/js/post.js
@@ -5,7 +5,19 @@ const setPostData = async () => {
     post: { author, body, created_at, title },
     related,
   } = await post("get_post", { id: postId });
-  console.log(response);
+  const titleHtml = document.querySelector(".postTitle");
+  const bodyHtml = document.querySelector("#body");
+  const authorHtml = document.querySelector(".postAuthor");
+  const dateHtml = document.querySelector(".postDate");
+  const createdYear = created_at.slice(0, 4);
+  const createdMonth = created_at.slice(5, 7);
+  const createdDay = created_at.slice(8, 10);
+
+  titleHtml.innerHTML = title;
+  bodyHtml.innerHTML = body;
+  authorHtml.innerHTML = author;
+  console.log(created_at);
+  dateHtml.innerHTML = `${createdDay}.${createdMonth}.${createdYear}`;
 };
 
 setPostData();
