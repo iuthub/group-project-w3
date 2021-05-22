700109ab05839d63cb34ca9da829225fa020299b
normuradoff <normuradoff1@gmail.com>
2021-05-22 18:36:29 +0500

    feat(js): separate helper functions for request to new file

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,39 +1,3 @@
-let BASE_URL = "http://ipblog.sbuy.uz/api/blog/";
-
-const olderButton = document.querySelector(".older");
-const newerButton = document.querySelector(".newer");
-
-const get = async (slug) => {
-  response = await fetch(BASE_URL + slug);
-  if (response.ok) {
-    return response.json();
-  } else
-    console.log(
-      new Error(
-        "Something went wrong in getting a response. Please, try again!."
-      )
-    );
-};
-
-const post = async (slug, body, options = {}) => {
-  response = await fetch(BASE_URL + slug, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-    ...options,
-  });
-  if (response.ok) {
-    return response.json();
-  } else
-    console.log(
-      new Error(
-        "Something went wrong in getting a response. Please, try again!."
-      )
-    );
-};
-
 const setCategories = async () => {
   const categoriesContainer = document.getElementById("categories");
   let { categories } = await get("get_categories");
@@ -42,7 +6,7 @@ const setCategories = async () => {
     category.className = "footer-link";
     const anchor = document.createElement("a");
     let query = new URLSearchParams(window.location.search);
-    query.set("category", id);
+    query.set("id", id);
     anchor.setAttribute("href", window.location.pathname + "?" + query);
     anchor.innerHTML = title;
     category.appendChild(anchor);
